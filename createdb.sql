cfcfc7a1c77d8eb5523e0536a71c322b7481cd29
Konstantin Udavikhin <udavconstantine@yandex.ru>
2018-06-09 01:12:18 +0300

    add createdb.sql

---
+++ createdb.sql
@@ -0,0 +1,54 @@
+CREATE UNLOGGED TABLE blocks (
+  "number" BIGINT PRIMARY KEY,
+  hash TEXT NOT NULL UNIQUE,
+  parentHash TEXT NOT NULL,
+  nonce TEXT NOT NULL,
+  sha3Uncles TEXT NOT NULL,
+  logsBloom TEXT NOT NULL,
+  transactionsRoot TEXT NOT NULL,
+  stateRoot TEXT NOT NULL,
+  receiptsRoot TEXT NOT NULL,
+  miner TEXT NOT NULL,
+  difficulty NUMERIC NOT NULL,
+  totalDifficulty NUMERIC NOT NULL,
+  size BIGINT NOT NULL,
+  proofOfAuthorityData TEXT NOT NULL,
+  gasLimit BIGINT NOT NULL,
+  gasUsed BIGINT NOT NULL,
+  "timestamp" bigint NOT NULL,
+  mixhash TEXT
+);
+
+CREATE UNLOGGED TABLE transactions (
+  hash TEXT PRIMARY KEY,
+  nonce BIGINT,
+  blockHash TEXT NOT NULL REFERENCES blocks(hash) ON DELETE CASCADE ON UPDATE CASCADE,
+  blockNumber BIGINT NOT NULL REFERENCES blocks("number") ON DELETE CASCADE ON UPDATE CASCADE,
+  transactionIndex BIGINT NOT NULL,
+  "from" TEXT NOT NULL,
+  "to" TEXT NOT NULL,
+  "value" NUMERIC NOT NULL,
+  gas BIGINT NOT NULL,
+  gasPrice NUMERIC NOT NULL,
+  "input" TEXT,
+  v TEXT,
+  r TEXT,
+  s TEXT
+);
+
+CREATE UNLOGGED TABLE uncles (
+  hash TEXT PRIMARY KEY,
+  blockNumber BIGINT NOT NULL REFERENCES blocks("number") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE VIEW view_last_block
+AS
+SELECT b.number
+FROM blocks b
+WHERE b.number = (SELECT max(b2.number) FROM blocks b2);
+
+CREATE INDEX idx_transactions_from
+ON transactions("from");
+
+CREATE INDEX idx_transactions_to
+ON transactions("to");
